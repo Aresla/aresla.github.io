a3f1a14978e40ba7720a76691ef6ef9b198b14b1
Aresla <areslas@list.ru>
2023-11-01 21:46:07 +0200

    add files

---
+++ script.js
@@ -0,0 +1,33 @@
+const checkbox = document.getElementById("ChangeTheme"); //get the checkbox to a variable
+
+//function for checkbox when checkbox is checked
+const darkmode = () => {
+    document.body.classList.add("dark-mode"); //add a class to the body tag
+    checkbox.checked = true; //set checkbox to be checked state
+    sessionStorage.setItem("mode", "dark"); //store a name & value to know that dark mode is on
+  }
+  
+  //function for checkbox when checkbox is not checked
+  const nodark = () => {
+    document.body.classList.remove("dark-mode"); //remove added class from body tag
+    checkbox.checked = false; //set checkbox to be unchecked state
+    sessionStorage.setItem("mode", "light"); //store a name & value to know that dark mode is off or light mode is on
+  }
+
+//check storage if dark mode was on or off
+if (sessionStorage.getItem("mode") == "dark") {
+  darkmode(); //if dark mode was on, run this funtion
+} else {
+  nodark(); //else run this funtion
+}
+
+//if the checkbox state is changed, run a funtion
+checkbox.addEventListener("change", () => {
+  //check if the checkbox is checked or not
+  if (checkbox.checked) {
+    darkmode(); //if the checkbox is checked, run this funtion
+  } else {
+    nodark(); //else run this funtion
+  }
+});
+
